b8725ed19d3e502f2a777a823067ab2d206d7246
shivakishore-gudi <shivakishoregudi8@gmail.com>
2025-09-24 23:29:10 +0530

    session

--- roboshop.sh
+++ roboshop.sh
@@ -11,4 +11,5 @@ if [ $instance != "frontend" ]; then
 else
      IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
 fi     
+    echo "$instance:$IP"
 done      
