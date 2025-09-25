75f3c7fe995bec6a0a7187754a11e643a67a86b9
shivakishore-gudi <shivakishoregudi8@gmail.com>
2025-09-25 17:49:07 +0530

    session

--- mongodb.sh
+++ mongodb.sh
@@ -1,10 +1,9 @@
-#!/bin/bash
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
-USERID=$(id -u)
+mkdir -p $LOGS_FOLDER
+echo "script started executed at: $(date)"
 
 if [ $USERID -ne 0 ]; then
     echo "ERROR:: please run these script with root privilage"
