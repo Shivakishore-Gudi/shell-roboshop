289ffbc89d8e4f05dd4c0474c909e2df252e6805
shivakishore-gudi <shivakishoregudi8@gmail.com>
2025-09-28 16:15:34 +0530

    session

--- catalogue-set.sh
+++ catalogue-set.sh
@@ -1,61 +1,62 @@
 #!/bin/bash
+
 set -euo pipefail
+
 trap 'echo "There is an error in $LINENO, Command is: $BASH_COMMAND"' ERR
 
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-LOGS_FOLDER="/var/log/shell-script"
+LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-MONGODB_HOST=mongodb.gskdaws.fun
-SCRIPT_DIR=$(cd $(dirname $0); pwd)
+SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.daws86s.fun
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 
 mkdir -p $LOGS_FOLDER
-echo "script started executed at: $(date)"
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
-USERID=$(id -u)
 if [ $USERID -ne 0 ]; then
-    echo "ERROR:: please run these script with root privilage"
-    exit 1
-fi    
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
+fi
 
-###NodeJS###
+##### NodeJS ####
 dnf module disable nodejs -y &>>$LOG_FILE
-dnf module enable nodejs:20 -y &>>$LOG_FILE
+dnf module enable nodejs:20 -y  &>>$LOG_FILE
 dnf install nodejs -y &>>$LOG_FILE
+echo -e "Installing NodeJS 20 ... $G SUCCESS $N"
 
-id roboshop
+id roboshop &>>$LOG_FILE
 if [ $? -ne 0 ]; then
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
 else
-    echo -e "user already exists ... $Y Skipping $N"
-fi 
+    echo -e "User already exist ... $Y SKIPPING $N"
+fi
 
-mkdir -p /app 
+mkdir -p /app
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
-cd /app
-
+cd /app 
 rm -rf /app/*
 unzip /tmp/catalogue.zip &>>$LOG_FILE
 npm install &>>$LOG_FILE
-
 cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
-
 systemctl daemon-reload
 systemctl enable catalogue &>>$LOG_FILE
-systemctl start catalogue
+echo -e "Catalogue application setup ... $G SUCCESS $N"
+
 cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
+dnf install mongodb-mongoshsfds -y &>>$LOG_FILE
 
-dnf install mongodb-mongosh -y &>>$LOG_FILE
-
-INDEX=$(mongosh --host mongodb.daws86s.fun --quiet --eval "db.adminCommand('listDatabases').databases.map(db => db.name).indexOf('catalogue')")
-if [ $INDEX -le 0 ];then
+INDEX=$(mongosh mongodb.daws86s.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
+if [ $INDEX -le 0 ]; then
     mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
 else
-    echo -e "catalogue products already loaded ... $Y Skipping $N "
-fi    
+    echo -e "Catalogue products already loaded ... $Y SKIPPING $N"
+fi
+
 systemctl restart catalogue
-echo "Restart catalogu ... $G Success $N 
+echo -e "Loading products and restarting catalogue ... $G SUCCESS $N"
