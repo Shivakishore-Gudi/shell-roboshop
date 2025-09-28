6211dd4836848b259e9d75d38ba69250cbcf4281
shivakishore-gudi <shivakishoregudi8@gmail.com>
2025-09-28 08:04:52 +0530

    session

--- catalogue.sh
+++ catalogue.sh
@@ -63,11 +63,12 @@ cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "copy mongo repo"
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "installing mongodb client"
-INDEX=$(mongosh mongodb.gskdaws.fun --quiet --eval "db.getmongo().getDBNames().indexof('catalogue')")
+INDEX=$(mongosh mongodb.daws86s.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
 if [ $INDEX -le 0 ];then
     mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
     VALIDATE $? "Load Catalogue products"
 else
     echo -e "catalogue products already loaded ... $Y Skipping $N"
+fi    
 systemctl restart catalogue
 VALIDATE $? "Restarted catalogue"
