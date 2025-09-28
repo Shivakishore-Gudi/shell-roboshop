35b223f73a8e5cc6adfff22db175e8c171d5f86e
shivakishore-gudi <shivakishoregudi8@gmail.com>
2025-09-28 16:52:07 +0530

    session

--- catalogue-set.sh
+++ catalogue-set.sh
@@ -51,7 +51,7 @@ echo -e "Catalogue application setup ... $G SUCCESS $N"
 cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 sudo systemd-resolve --flush-caches 2>/dev/null || true #flush the previous IP address in cache
-
+echo "flushed"
 INDEX=$(mongosh mongodb.gskdaws.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
 if [ $INDEX -le 0 ]; then
     mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
