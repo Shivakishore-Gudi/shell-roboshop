04c7052fa3f3c874a52cb7b3df22c298123ec2c8
shivakishore-gudi <shivakishoregudi8@gmail.com>
2025-09-28 11:33:47 +0530

    session

--- catalogue-set.sh
+++ catalogue-set.sh
@@ -51,7 +51,7 @@ cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
 
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 
-INDEX=$(mongosh mongodb.daws86s.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
+INDEX=$(mongosh --host mongodb.daws86s.fun --quiet --eval "db.adminCommand('listDatabases').databases.map(db => db.name).indexOf('catalogue')")
 if [ $INDEX -le 0 ];then
     mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
 else
